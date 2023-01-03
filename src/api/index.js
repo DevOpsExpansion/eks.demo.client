f3b309c18f20cfae08b7666c56b1678d79fad600
Shurgentum <shurgentum@gmail.com>
2023-01-03 06:31:20 +0200

    Backend endpoint - passed BASE_URL to application build

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000" });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
